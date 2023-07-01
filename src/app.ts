0c9d04ca223e2780b8f542d7e0aa5648cd531796
haritiwari4 <haritiwariofficial@gmail.com>
2023-07-01 14:48:53 +0530

    changes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
-import express from 'express';
-import productRoutes from './routes/productRoutes';
+import express from "express";
+import productRoutes from "./routes/productRoutes";
 
 const app = express();
 
 app.use(express.json());
-app.use('/products', productRoutes);
+app.get("/", (req, res, next) => {
+  res.json({ message: "hello world" });
+});
+app.use("/products", productRoutes);
 
 export default app;
